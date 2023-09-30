f40fd51636e74604a9ecc2e4a7d04375fe3d2612
Wesley  Webb <wesleywebb@Wesleys-MBP.lan>
2023-09-29 21:07:59 -0700

    ITS WORKING

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import MyGodotGame from './components/GameComponent';
+import MyGodotGame from './components/MyGodotGame';
 import useTest from './hooks/useTest';
 
 const App = () => {
 
   const {opponentName, error} = useTest();
   return <div>
-    {/* <div>Game Component against model: {error ? "NOT VALID" : opponentName}</div> */}
+    <div>Game Component against model: {error ? "NOT VALID" : opponentName}</div>
     <MyGodotGame />
   </div>
   // return <GameComponent />
---
+++ src/components/MyGodotGame.js
@@ -0,0 +1,16 @@
+
+const MyGodotGame = () => {
+    return (
+    <div style={{width: "99.7vw", height: "95vh"}}>
+        <iframe
+            title="Godot Game"
+            src="godot/asdf.html"  // Path to your Godot-generated HTML file
+            // width="1500"
+            width="100%"
+            height="100%"
+        />
+    </div>
+    );
+}
+
+export default MyGodotGame;
